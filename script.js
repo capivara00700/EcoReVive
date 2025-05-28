ce0254bc724913da53074b9aa6a07ccc6a3eb7a5
capivara00700 <155832965+capivara00700@users.noreply.github.com>
2025-05-28 12:30:45 -0300

    Update script.js

--- script.js
+++ script.js
@@ -6,12 +6,10 @@ $('.hamburguer').on('click', () => {
 //Menu lateral
 $('.hamburguer').on('click', () => {
     if($('.navbar-nav').hasClass('active')){
-        $('.navbar-nav').animate({'right': '-50vw'}, 'medio')
+        $('.navbar-nav').animate({'left': '-50vw'}, 'medio')
         $('.navbar-nav').removeClass('active')
-        $('html').css({'position': 'relative'})
     } else {
-        $('.navbar-nav').animate({'right': '0px'}, 'medio')
+        $('.navbar-nav').animate({'left': '0px'}, 'medio')
         $('.navbar-nav').addClass('active')
-        $('html').css({'position': 'fixed'})
     }
 })

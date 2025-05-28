2aca07d1fe42a87c197ff28eea0a653a33510013
capivara00700 <155832965+capivara00700@users.noreply.github.com>
2025-05-28 04:35:24 -0300

    Update script.js

--- script.js
+++ script.js
@@ -8,8 +8,10 @@ $('.hamburguer').on('click', () => {
     if($('.navbar-nav').hasClass('active')){
         $('.navbar-nav').animate({'right': '-50vw'}, 'medio')
         $('.navbar-nav').removeClass('active')
+        $('html').css({'position': 'relative'})
     } else {
         $('.navbar-nav').animate({'right': '0px'}, 'medio')
         $('.navbar-nav').addClass('active')
+        $('html').css({'position': 'fixed'})
     }
 })

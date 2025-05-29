1db756c1c757c825c089049e92f1cf225b0a5836
capivara00700 <155832965+capivara00700@users.noreply.github.com>
2025-05-29 18:08:32 -0300

    Update script.js

--- script.js
+++ script.js
@@ -14,6 +14,21 @@ $('.hamburguer').on('click', () => {
     }
 })
 
+//menu links
+$('.nav-link').on('click', () => {
+    if(tamanho < 992){
+        $('.navbar').toggleClass('active')
+
+        if($('.navbar-nav').hasClass('active')){
+            $('.navbar-nav').animate({'left': '-50vw'}, 'medio')
+            $('.navbar-nav').removeClass('active')
+        } else {
+            $('.navbar-nav').animate({'left': '0px'}, 'medio')
+            $('.navbar-nav').addClass('active')
+        }
+    }
+})
+
 //Conteudo principal
 let janela = $('section').find('.reverse')
 let tamanho = innerWidth
@@ -23,3 +38,17 @@ if(tamanho < 992){
 } else {
     $(janela).removeClass('flex-column-reverse')    
 }
+
+//Animação opacidade
+const myObserver = new IntersectionObserver( entries => {
+    entries.forEach( entry => {
+        if(entry.isIntersecting){
+            entry.target.classList.add('show')
+        } else {
+            entry.target.classList.remove('show')
+        }
+    } ) 
+} )
+
+const elements = document.querySelectorAll('.hidden')
+elements.forEach( element => myObserver.observe(element))
